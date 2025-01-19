c7d4b154e8b18417aa4d04205ff8965e5cfe7cfc
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2025-01-19 18:12:33 +0600

    created the basic navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 const Navbar = () => {
     return (
-        <div>
+        // main div
+        <div className="flex justify-between">
             {/* Routes */}
             <div>
                 <ul className="flex gap-4">
@@ -15,7 +16,14 @@ const Navbar = () => {
                 <img src="/src/assets/Screenshot from 2025-01-19 18-05-47.png" alt="" />
             </div>
             {/* Account */}
-            
+            <div className="flex">
+                <div>
+                    <img src="/src/assets/icons8-cart-64.png" alt="" />
+                </div>
+                <div>
+                    <img src="/src/assets/icons8-profile-picture-60.png" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
