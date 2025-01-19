662eca6100f9e073308d9a8b9b1cb2d38e64768f
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2025-01-19 18:21:03 +0600

    Navbar all routes logo and account creatd and designed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,29 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
     return (
         // main div
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center">
             {/* Routes */}
             <div>
                 <ul className="flex gap-4">
-                    <li>Home</li>
-                    <li>Shop</li>
-                    <li>About</li>
-                    <li>Contacts</li>
+                    <NavLink to={"/"}>Home</NavLink>
+                    <NavLink to={"/"}>Shop</NavLink>
+                    <NavLink to={"/"}>About</NavLink>
+                    <NavLink to={"/"}>Contacts</NavLink>
                 </ul>
             </div>
             {/* Logo */}
             <div>
-                <img src="/src/assets/Screenshot from 2025-01-19 18-05-47.png" alt="" />
+                <img className="h-16 pe-[150px]" src="/src/assets/Screenshot from 2025-01-19 18-05-47.png" alt="" />
             </div>
             {/* Account */}
-            <div className="flex">
+            <div className="flex gap-2">
                 <div>
-                    <img src="/src/assets/icons8-cart-64.png" alt="" />
+                    <img className="h-10" src="/src/assets/icons8-cart-64.png" alt="" />
                 </div>
                 <div>
-                    <img src="/src/assets/icons8-profile-picture-60.png" alt="" />
+                    <img className="h-10" src="/src/assets/icons8-profile-picture-60.png" alt="" />
                 </div>
             </div>
         </div>
